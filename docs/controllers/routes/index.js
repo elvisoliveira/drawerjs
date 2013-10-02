66c4f37626aa626518adb288a5c3e7aeab715a9d
Roland <rolandjitsu@gmail.com>
2013-10-02 09:37:31 +0200

    migrated the response.locals to the index controller instead of making it accessible to all controllers

--- docs/controllers/routes/index.js
+++ docs/controllers/routes/index.js
@@ -2,9 +2,12 @@
  * Index Route Controller
  */
 
-var flag = "routes" + require("path").sep,
+var path = require("path"),
+	config = require(path.resolve(process.cwd(), "config.js")),
+	flag = "routes" + path.sep,
 	index = __filename.indexOf(flag);
 
-module.exports = function (request, response) {
+module.exports =  function (request, response) {
+	response.locals = config;
 	return response.render(__filename.slice(index + flag.length, __filename.length).replace(".js", ""));
 };
